ba45a8760cd1d3b7d4ac53f8d54d9161caea0da9
Jens Jahnke <jan0sch@gmx.net>
2012-02-19 08:16:42 +0100

    Changed SQL for getting event stripes to make end_date optional.

--- lib/event_calendar.rb
+++ lib/event_calendar.rb
@@ -66,7 +66,7 @@ module EventCalendar
     def events_for_date_range(start_d, end_d, find_options = {})
       self.scoped(find_options).find(
         :all,
-        :conditions => [ "(? <= #{self.quoted_table_name}.#{self.end_at_field}) AND (#{self.quoted_table_name}.#{self.start_at_field}< ?)", start_d.to_time.utc, end_d.to_time.utc ],
+        :conditions => [ "((? <= #{self.quoted_table_name}.#{self.end_at_field}) OR (#{self.quoted_table_name}.#{self.end_at_field} IS NULL)) AND (#{self.quoted_table_name}.#{self.start_at_field}< ?)", start_d.to_time.utc, end_d.to_time.utc ],
         :order => "#{self.quoted_table_name}.#{self.start_at_field} ASC"
       )
     end
